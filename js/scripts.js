e041aba8f0ea6853d0358f25917f1d8b7ff69d24
Kristen Kulha and snow vilay <snow@>
2017-08-15 10:46:05 -0700

    Add initial form for user input with functionality.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("#wordForm").submit(function(event){
+    var userSentence = $("#sentenceInput").val();
+    var codeSentence = [];
+    for(var i = 0; i < userSentence.length; i++){
+      var currentChar = userSentence.charAt(i);
+      if(currentChar === "a" || currentChar === "e" || currentChar === "i" || currentChar === "o" || currentChar === "u"){
+        codeSentence.push("-");
+      }else {
+        codeSentence.push(userSentence.charAt(i));
+      }
+    };
+    newSentence = codeSentence.join("");
+    console.log(newSentence);
+    event.preventDefault();
+  });
+});
